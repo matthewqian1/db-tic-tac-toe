0466959d8e7f33319a7181107b1a98954a5dba32
Matthew <matthew.qian2@gmail.com>
2024-08-22 17:18:00 +1000

    add draw test cases

--- src/test/java/TestTicTacToe.java
+++ src/test/java/TestTicTacToe.java
@@ -5,6 +5,46 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestTicTacToe {
 
     @Test
+    public void testDraw1() {
+        char[][] board = {
+                {'X', '-', 'O'},
+                {'X', '-', 'X'},
+                {'O', '-', 'X'},
+
+        };
+
+        assertEquals(empty, getResult(board));
+
+    }
+
+
+    @Test
+    public void testDraw2() {
+        char[][] board = {
+                {'X', 'O', 'X'},
+                {'O', 'X', 'O'},
+                {'O', 'X', 'O'},
+
+        };
+
+        assertEquals(empty, getResult(board));
+
+    }
+
+    @Test
+    public void testDraw3() {
+        char[][] board = {
+                {'-', 'O', '-'},
+                {'O', '-', 'O'},
+                {'-', 'X', 'O'},
+
+        };
+
+        assertEquals(empty, getResult(board));
+
+    }
+
+    @Test
     public void testRowWin() {
         char[][] board = {
                 {'X', 'O', 'O'},
